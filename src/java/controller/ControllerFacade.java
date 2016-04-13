aaa92306083a9cf0bf7335237cfb7f8e23fc03e5
Juan Sotomayor <jusoto@gmail.com>
2016-04-13 11:58:59 -0400

    Added files via upload

---
+++ src/java/controller/ControllerFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author Juan
+ */
+public class ControllerFacade {
+    
+    public ControllerFacade(){
+        
+    }
+    
+    public void RegisterClient(){
+        
+    }
+    
+    public void RegisterTower(){
+        
+    }
+    
+    public void RequestService(){
+        
+    }
+    
+}
---
+++ src/java/model/ModelFacade.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Juan
+ */
+public class ModelFacade {
+    
+    public ModelFacade(){
+        
+    }
+    
+    public void CreateClient(){
+        
+    }
+    
+    public void CreateTower(){
+        
+    }
+    
+    public void CreateService(){
+        
+    }
+    
+    public void UpdateService(){
+        
+    }
+    
+    
+}
